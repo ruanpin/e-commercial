c408c948204cb789a2a826ba343c7f901e5a333d
vincent <oa51360@gmail.com>
2022-09-14 16:01:15 +0800

    updating Cart component

--- src/pages/Cart/index.vue
+++ src/pages/Cart/index.vue
@@ -1,13 +1,71 @@
 <template>
-  <div>123</div>
+  <div class="cart-container">
+      <div class="title-area">
+            <div class="left">
+                <div class="checkbox">
+                    <input type="checkbox" id="allChecked" >
+                    <label for="allChecked">全選</label>
+                </div>
+                <div class="title">商品</div>
+            </div>
+          
+            <div class="right">
+                <div class="title">單價</div>
+                <div class="title">數量</div>
+                <div class="title">總計</div>
+                <div class="title">刪除</div>
+            </div>
+          
+      </div>
+      <div class="cart-panel">
+
+      </div>
+  </div>
 </template>
 
 <script>
     export default {
-
+        name:'Cart',
     }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    // 以下通用-----------------------
+    input {
+        width: 1.2rem;
+        height: 1.2rem;
+        appearance: checkbox;
+        margin-right:0.3rem;
+    }
+    // 以上通用-----------------------
 
+    .cart-container {
+        display:flex;
+        flex-direction: column;
+        background-color: rgb(174, 212, 158);
+        .title-area {
+            display:flex;
+            align-items: center;
+            justify-content: space-between;
+            padding:1.5em;
+            .left {
+                display:flex;
+                align-items: center;
+                .checkbox {
+                    display:flex;
+                    align-items: center;
+                    margin-right:1.5em;
+                }
+            }
+            .right {
+                display:flex;
+                align-items: center;
+                width: 50%;
+                .title {
+                    width: 25%;
+                    text-align: center;
+                }
+            }
+        }
+    }
 </style>
