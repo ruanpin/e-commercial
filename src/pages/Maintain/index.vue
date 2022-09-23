7d8d47a2ccec28cf981f943bdc851b90bc5b2b9b
vincent <oa51360@gmail.com>
2022-09-23 16:29:15 +0800

    updating Maintain component.

--- src/pages/Maintain/index.vue
+++ src/pages/Maintain/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="Maintain-container">Maintain</div>
+  <div class="Maintain-container">
+    <h1>礦石保養方法</h1>
+    <h2>清除表面髒汙</h2>
+    <ul>
+      <li>1. 用軟毛刷輕刷表面灰塵，適用日常保養</li>
+      <li>2. 清水沖洗，勿使用化學品清洗或浸泡，以免破壞晶體</li><br>
+    </ul>
+    <h2>淨化礦石</h2>
+    <ul>
+      <li>1. 陽光消磁</li>
+      <p>陽光充足時，讓晶石接受陽光照射30分鐘至60分鐘，即可達到凈化、加強晶石能量的效果。</p>
+      <p>有色水晶較不適用此法，水晶在強烈陽光照射或日曬時間過長容易造成色澤容易變淡</p>
+      <p>溫和的陽光適用大部分水晶，如白水晶、黃水晶、茶水晶..等較耐高溫的晶石。</p>
+      <li>2. 流水消磁</li>
+      <p>水晶放在未受污染的自然水流 (山泉、溪水等)約10分鐘，蘊含大自然能量的流水可為晶石淨化並增加能量，再放置陰涼處自然風乾即可。</p>
+      <li>3. 水晶缽消磁法</li>
+      <p>將水晶礦石置於水晶缽內，敲擊水晶缽使其發出聲波，藉由聲波震動淨化水晶磁場</p>
+      <li>4. 香熏凈化</li>
+      <p>點上沉香、檀香、鼠尾草或香柏草等具有淨化能力的藥草，讓淨化煙在水晶周圍環繞，即可達到淨化效果。</p>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -10,6 +30,25 @@ export default {
 
 <style lang="scss" scoped>
     .Maintain-container {
-
+      font-size:1.3rem;
+      line-height:1.8rem;
+      h1 {
+        font-weight: 700;
+        font-size:2rem;
+        text-align: center;
+        margin-top:2rem;
+        margin-bottom:3rem;
+      }
+      p, ul {
+        margin-bottom:3.5rem;
+        li {
+          margin-bottom:1.5rem;
+        }
+      }
+      h2 {
+        font-weight: 700;
+        font-size:1.7rem;
+        margin-bottom:1.8rem;
+      }
     }
 </style>
