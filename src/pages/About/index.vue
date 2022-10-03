8017047ef43b3f141c149b15342848fbfab3a644
vincent <oa51360@gmail.com>
2022-10-03 18:30:00 +0800

    adding animation effect.

--- src/pages/About/index.vue
+++ src/pages/About/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="about-container">
     <img src="/about/about.jpg">
-    <div class="text">
-      <h1>品牌簡介</h1>
-      <p>於 2007 年的義大利成立，由於創辦人對礦物工藝的喜愛，奠定了 crystal品牌特殊的工藝風格，傳遞礦物本身優雅氣質</p>
-      
-      <p>「在礦石的世界，就連 1mm 的差異也是無法退讓的堅持。」－ crystal 創辦人</p>
-    </div>
+    <transition name="showup">
+      <div class="text" v-show="isAboutShow">
+        <h1>品牌簡介</h1>
+        <p>於 2007 年的義大利成立，由於創辦人對礦物工藝的喜愛，奠定了 crystal品牌特殊的工藝風格，傳遞礦物本身優雅氣質</p>
+        
+        <p>「在礦石的世界，就連 1mm 的差異也是無法退讓的堅持。」－ crystal 創辦人</p>
+      </div>
+    </transition>
+    
 
   </div>
 </template>
@@ -14,6 +17,28 @@
 <script>
 export default {
     name:'About',
+    data(){
+      return {
+        isAboutShow:false,
+
+      }
+    },
+    methods:{
+      showItem(){
+        // console.log(window.pageYOffset)
+        //170
+        if (window.pageYOffset > 100) {
+          this.isAboutShow = true
+        }
+      }
+    },
+    mounted(){
+      window.addEventListener('scroll',this.showItem)
+    },
+    beforeDestroy(){
+      window.removeEventListener('scroll',this.showItem)
+    }
+    
 }
 </script>
 
@@ -21,12 +46,14 @@ export default {
   .about-container {
     width: 100%;
     font-size: 1.3rem;
+    height: 900px;
     img {
       width: 100%;
     }
     .text {
       margin-top: 5rem;
       margin-bottom: 8rem;
+
       h1 {
         font-weight: 500;
         font-size:1.8rem;
@@ -40,5 +67,18 @@ export default {
       }
     }
     
+  }
+  @keyframes showUp {
+    from {
+      transform: translateY(15%);
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0%);
+      opacity: 1;
+    }
+  }
+  .showup-enter-active {
+    animation: showUp 1s;
   }
 </style>
